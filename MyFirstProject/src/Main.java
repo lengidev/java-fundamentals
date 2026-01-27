f4dc358992094d34cc0db8b8d4f38c2af3ec5ae8
Lenganji Sinyangwe <sinyangwejunior17@gmail.com>
2026-01-27 15:43:53 +0200

    "My First Java Program"

---
+++ MyFirstProject/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args){
+
+        //THIS IS MY FIRST JAVA PROGRAM
+
+        /*
+            This
+            is
+            a
+            Multiline
+            Comment
+        */
+
+        System.out.print("I like Chicken \n");
+        System.out.println("It's really good!");
+        System.out.print("Buy me Pizza!");
+        //use a "sout" to write System.out.println() faster
+    }
+}
