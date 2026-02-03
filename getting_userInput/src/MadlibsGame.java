7dda66067e07f87e36b68f598f54fca1789e9d6b
Lenganji Sinyangwe <sinyangwejunior17@gmail.com>
2026-02-03 17:22:15 +0200

    Finished code on Madlibs game.

---
+++ getting_userInput/src/MadlibsGame.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class MadlibsGame {
+    public static void main (String[] args){
+        // MAD LIBS GAME
+
+        Scanner scanner = new Scanner(System.in);
+
+        String adjective1, noun1, adjective2 , verb1, adjective3;
+
+        System.out.print("Enter an adjective (description): ");
+        adjective1 = scanner.nextLine();
+
+        System.out.print("Enter a noun (name or person): ");
+        noun1 = scanner.nextLine();
+
+        System.out.print("Enter an adjective (description): ");
+        adjective2= scanner.nextLine();
+
+        System.out.print("Enter a verb ending with -ing (an action): ");
+        verb1 = scanner.nextLine();
+
+        System.out.print("Enter an adjective (description): ");
+        adjective3 = scanner.nextLine();
+
+        System.out.println("\n Today I went to a " + adjective1 + " zoo.");
+        System.out.println("In an exhibit, I saw a " + noun1 + ".");
+        System.out.println(noun1 + " was " + adjective2 + " and " + verb1 + "!");
+        System.out.println("I was " + adjective3 + "!");
+
+        scanner.close();
+    }
+}
